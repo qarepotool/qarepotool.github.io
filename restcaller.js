9989a0db349c359f2b1a7aed249221daf808905c
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-03 18:41:20 +0100

    idboardasvariable

--- restcaller.js
+++ restcaller.js
@@ -18,7 +18,8 @@
     console.log(this.responseText);
   }
     });
-    xhr.open("POST", "https://api.trello.com/5b61a1ff8d7233289bbb1ef0/cards");    
+    var idboard="5b61a1ff8d7233289bbb1ef0";
+    xhr.open("POST", "https://api.trello.com/"+idboard+"/cards");    
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     var data1 = {};
     data1.key = "John";
