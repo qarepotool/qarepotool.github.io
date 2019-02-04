29959e4706e1ac36649207c4720125976fe62281
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-04 22:17:56 +0100

    form1

---
+++ test1.js
@@ -0,0 +1,6 @@
+(function() {
+  var e;
+  let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+  width=300,height=300,left=100,top=100`;
+  window.open("./form/form.html", name, params)
+}).call(this);
