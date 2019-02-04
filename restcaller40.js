0885cb6dfc21383214d283fd338667f9c24b8ba9
Jose Agullo <jose.agullo@mtp.es>
2019-02-04 14:46:45 +0100

    test

--- restcaller40.js
+++ restcaller40.js
@@ -58,7 +58,7 @@
     arr[1][1]=document.querySelector('#customfield_10476-val').textContent;
     //arr[1][1]=document.getElementById('customfield_10476-val').value;
     
-    arr[1][2]=document.getElementById('customfield_10361-val').nodeValue;
+    arr[1][2]=document.getElementById('customfield_10361-val').textContent;
     arr[1][3]=document.getElementById('customfield_10363-val').textContent;
     arr[1][4]=document.getElementById('customfield_10806-val').textContent;
     arr[1][5]=document.getElementById('customfield_11408-val').textContent;
