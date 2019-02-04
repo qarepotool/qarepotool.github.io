c154b4e9791dd7a029d9d941738b3827c98f8a16
Jose Agullo <jose.agullo@mtp.es>
2019-02-04 15:04:46 +0100

    test

--- restcaller42.js
+++ restcaller42.js
@@ -55,8 +55,10 @@
     arr[0][14]="5c53200fc348ab7ac13488bb";
     arr[0][15]="5c532017aa78c45fabead47e";
     arr[1][0]=document.getElementById('key-val').textContent;
-    arr[1][1]=document.querySelector('customfield_10476-val').textContent;
-    //arr[1][1]=document.getElementById('customfield_10476-val').value;
+    //arr[1][1]=document.querySelector('customfield_10476-val').textContent;
+
+    arr[1][1]=document.getElementById('customfield_10476-val').value;
+    alert( arr[1][1]);
     
     arr[1][2]=document.getElementById('customfield_10361-val').textContent;
     arr[1][3]=document.getElementById('customfield_10363-val').textContent;
