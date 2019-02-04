79ea5ffe8b4deb902285ce0074765aaf4f98e16b
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-04 22:48:40 +0100

    test

--- test4.js
+++ test4.js
@@ -1,6 +1,6 @@
 (function() {
   var e;
   let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-  width=300,height=300,left=100,top=100`;
+  width=350,height=400,left=100,top=100`;
   window.open("https://qarepotool.github.io/form/form.html", name, params)
 }).call(this);
