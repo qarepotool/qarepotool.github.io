d951da7f1f1250ed5b49290d6e2d9b47496648ca
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-04 22:27:22 +0100

    form

--- test3.js
+++ test3.js
@@ -2,5 +2,5 @@
   var e;
   let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
   width=300,height=300,left=100,top=100`;
-  window.open("https://cdn.jsdelivr.net/gh/qarepotool/qarepotool.github.io/form/form.html", name, params)
+  window.open("https://qarepotool.github.io/form/form.html", name, params)
 }).call(this);
