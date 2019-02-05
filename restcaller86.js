08613793e316a8030e56611e9425b33ba017f92d
Jose Agullo <jose.agullo@mtp.es>
2019-02-05 09:19:46 +0100

    test

--- restcaller86.js
+++ restcaller86.js
@@ -126,8 +126,7 @@
     if (document.getElementById('customfield_10392-val') == null) {
       console.log("Este campo es null " + arr[1][13]);
     } else {
-      document.getElementById('aui-uid-3').click;
-      arr[1][13] = document.getElementById('customfield_10392-val').innerText.trim();
+      arr[1][13] = document.getElementById('customfield_10392-val').textContent;
     }
     if (document.getElementById('customfield_10361-val') == null) {
       console.log("Este campo es null " + arr[1][14]);
