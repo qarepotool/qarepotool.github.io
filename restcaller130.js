9464d880e6e1f6b0bf1f50210eee2d0b5569c0fb
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-05 19:56:13 +0100

    reformat

--- restcaller130.js
+++ restcaller130.js
@@ -1,5 +1,10 @@
 var type_delivery=null;
 var tareaomnia=null;
+var idList=null;
+var key=null;
+var token=null;
+var arr = Create2DArray(16);
+var cardcreated=null;
 (function () {
   var e;
   var ventana;
@@ -10,75 +15,19 @@ var tareaomnia=null;
     e.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js');
     document.body.appendChild(e);
   }
-  window.saveTrelloCard = function (idList, key, token) {
+  window.saveTrelloCard = function (idList1, key1, token1) {
     var data = null;
+    idList=idList1;
+    key=key1;
+    token=token1;
     var xhr = new XMLHttpRequest();
     xhr.addEventListener("readystatechange", function () {
       if (this.readyState === this.DONE) {
         console.log(this.responseText);
       }
     });
-    
   };
-  let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-    width=350,height=400,left=100,top=100`;
-    ventana=window.open("https://qarepotool.github.io/form/form.html", "Datos propios", params);
-}).call(this);
-
-function evaluatevalues(event){
-  if (document.getElementById('element_2').value == "") {
-   alert ("Tipo de entrega no informado");
- } 
- else {
-   type_delivery=document.getElementById('element_2').value;
-   console.log("Type of delivery: " + type_delivery);
-   createtrello(type_delivery,tareaomnia);
-   window.close(); 
- }
-  
-  //ventana.close;
- }
-
-  function createtrello(type_delivery,tareaomnia) {
-  var e;
-  var ventana;
-  if (!(typeof jQuery !== "undefined" && jQuery !== null)) {
-    e = document.createElement('script');
-    e.setAttribute('type', 'text/javascript');
-    e.setAttribute('charset', 'UTF-8');
-    e.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js');
-    document.body.appendChild(e);
-  }
-  window.saveTrelloCard = function (idList, key, token) {
-    var data = null;
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === this.DONE) {
-        console.log(this.responseText);
-      }
-    });
-    //CREACION CARD ID
-    var data1 = {};
-    xhr.open("POST", "https://api.trello.com/1/cards", false);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    data1.key = key;
-    data1.token = token;
-    data1.name = document.title;
-    data1.desc = document.location.href;
-    data1.idList = "5b61b58259e21d8a1c18a247";
-    var json2 = JSON.stringify(data1);
-    xhr.send(json2)
-    //console.log(xhr.responseText);
-    //console.log(xhr.readyState);
-    //console.log(xhr.status);
-    var jsonvalue = JSON.parse(xhr.responseText);
-    var cardcreated = jsonvalue['id'];
-    Addlabeltocard(1,cardcreated,key,token);
-    
     // CREACION DE ARRAY DE CUSTOM FIELDS
-    
-    var aux = "";
-    var arr = Create2DArray(16);
     arr[0][0] = "5c531ee01f9d420689bcd3e3";
     arr[0][1] = "5c531f09f4c44165b6600933";
     arr[0][2] = "5c531f14d53bd95e9223671e";
@@ -154,7 +103,7 @@ function evaluatevalues(event){
       console.log("Este campo es null " + arr[1][11]);
     } else {
       arr[1][11] = document.getElementById('customfield_10366-val').textContent.trim();
-      Addduetocard(arr[1][11],cardcreated,key,token);
+   //   Addduetocard(arr[1][11],cardcreated,key,token);
     }
     if (document.getElementById('description-val') == null) {
       console.log("Este campo es null " + arr[1][12]);
@@ -175,6 +124,49 @@ function evaluatevalues(event){
       console.log("Este campo es null " + arr[1][15]);
     } else {
       arr[1][15] = aux = document.getElementById('customfield_10361-val').textContent.trim();
+    }
+  let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+    width=350,height=400,left=100,top=100`;
+    ventana=window.open("https://qarepotool.github.io/form/form.html", "Datos propios", params);
+}).call(this);
+
+function evaluatevalues(event){
+  if (document.getElementById('element_2').value == "") {
+   alert ("Tipo de entrega no informado");
+ } 
+ else {
+   type_delivery=document.getElementById('element_2').value;
+   tareaomnia=document.getElementById('element_1').value;
+   console.log("Type of delivery: " + type_delivery);
+   createtrello(type_delivery,tareaomnia);
+   window.close(); 
+ }
+ }
+
+  function createtrello(type_delivery,tareaomnia) {
+  var e;
+  var ventana;
+    //CREACION CARD ID
+    var data1 = {};
+    xhr.open("POST", "https://api.trello.com/1/cards", false);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    data1.key = key;
+    data1.token = token;
+    data1.name = document.title;
+    data1.desc = document.location.href;
+    data1.idList = "5b61b58259e21d8a1c18a247";
+    var json2 = JSON.stringify(data1);
+    xhr.send(json2)
+    //console.log(xhr.responseText);
+    //console.log(xhr.readyState);
+    //console.log(xhr.status);
+    var jsonvalue = JSON.parse(xhr.responseText);
+    cardcreated = jsonvalue['id'];
+    Addlabeltocard(1,cardcreated,key,token);
+    if (arr[1][11] = "") {
+      console.log("Este campo es null " + arr[1][11]);
+    } else {
+      Addduetocard(arr[1][11],cardcreated,key,token);
     }
     // CREACION DE UPDATE DE CUSTOM FIELDS
     for (var j = 0; j < 16; j++) {
@@ -197,8 +189,6 @@ function evaluatevalues(event){
 
     //console.log(json3);
   };
-}
-
 
 function Create2DArray(rows) {
   var arr = [];
