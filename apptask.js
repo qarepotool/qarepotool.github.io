9e316513dc3a726537a98a3188cd46fae442874b
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-08 23:11:44 +0100

    df

---
+++ apptask.js
@@ -0,0 +1,790 @@
+apptask=[
+ {
+   'CTname': 'AceReport',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'AceReport Linguistic Engine',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Acrobat Security Patch',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'ADM - Administration Module',
+   'OmniaTask': '68800: Correctives ADM Administration Module'
+ },
+ {
+   'CTname': 'AdminDocs',
+   'OmniaTask': '63172: Correctives ShareDox'
+ },
+ {
+   'CTname': 'AdminDocs Admin Tool',
+   'OmniaTask': '63172: Correctives ShareDox'
+ },
+ {
+   'CTname': 'AG Support Tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Agorateka',
+   'OmniaTask': '68513: Correctives Agorateka'
+ },
+ {
+   'CTname': 'Aleph / Documarc (Library management tool)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Alista',
+   'OmniaTask': '65906: Correctives TM Class'
+ },
+ {
+   'CTname': 'Anti-Counterfeiting Intelligence Support Tool',
+   'OmniaTask': '65053: Correctives Observatory'
+ },
+ {
+   'CTname': 'Anti-Counterfeiting Rapid Intelligence System',
+   'OmniaTask': '65053: Correctives Observatory'
+ },
+ {
+   'CTname': 'ARCAD (Administrative Archiving Process Tool)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'B2B e-filing',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Boards of Appeal Single Tool',
+   'OmniaTask': '65836: Correctives BOAST'
+ },
+ {
+   'CTname': 'Business Integration Layer',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Business Intelligence System (BO)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CAST - Contract Agents Search Tool ',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Certificates & Publication',
+   'OmniaTask': '65900: Correctives PUB'
+ },
+ {
+   'CTname': 'CF111 Search Image',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF112 TM View',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF113 Classification',
+   'OmniaTask': '65906: Correctives TM Class'
+ },
+ {
+   'CTname': 'CF114 Seniority',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF1210 Cesto',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF1211 Common Gateway for Applications',
+   'OmniaTask': '66983: Correctives Common Gateway'
+ },
+ {
+   'CTname': 'CF1212 Common Quality Management System',
+   'OmniaTask': '66983: Correctives Common Gateway'
+ },
+ {
+   'CTname': 'CF125 Quality Standards,\'66983: Correctives Common Gateway',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF126 Forecasting Tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF126 Harmonized Forecasting Methodology',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF127 User Satisfaction Survey',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF128 Designview',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF214 FSP Back-Office TM',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF21419 SP TM e-Filing - Adoptions',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CF320 eLearning',
+   'OmniaTask': '65828: Correctives EUIPO Academy Learning Portal'
+ },
+ {
+   'CTname': 'Clarity PPM for Project & Portfolio Management',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Common Call Centre Tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Common Classification Database of G&S',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Common mail dispatch (QFMan)',
+   'OmniaTask': '66094: Correctives QFMan'
+ },
+ {
+   'CTname': 'Common Mailroom System (CMS)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Common Payment System (CPS)',
+   'OmniaTask': '68801: Correctives CPS Common Payment System'
+ },
+ {
+   'CTname': 'Common Payment Tool (COPATO)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Common Query tool (CQT)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Confluence',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Correspondence (COR)',
+   'OmniaTask': '64619: Correctives COR'
+ },
+ {
+   'CTname': 'CSS Framework',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CTM Community Search System',
+   'OmniaTask': '66550: Correctives CTM Community Search System'
+ },
+ {
+   'CTname': 'CTM Database Extraction Engine',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CTM Download',
+   'OmniaTask': '67524: Correctives CTM Download'
+ },
+ {
+   'CTname': 'CTM e-opposition',
+   'OmniaTask': '71650: Correctives Inter Partes eFiling'
+ },
+ {
+   'CTname': 'CTM eFiling',
+   'OmniaTask': '65832: Correctives TM eFiling'
+ },
+ {
+   'CTname': 'CTM eRenewal',
+   'OmniaTask': '66678: Correctives Harmonized eFiling'
+ },
+ {
+   'CTname': 'CTM Euroace - internal',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CTM Euroace Online',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CTM Key-In',
+   'OmniaTask': '65832: Correctives TM eFiling'
+ },
+ {
+   'CTname': 'CTM Madrid Protocol System (MPS)',
+   'OmniaTask': '68802: Correctives MPS Madrid Protocol System'
+ },
+ {
+   'CTname': 'CTM Madrid Protocol System - Seniorities (MPS-SEN)',
+   'OmniaTask': '68802: Correctives MPS Madrid Protocol System'
+ },
+ {
+   'CTname': 'CTM Online',
+   'OmniaTask': '65049: Correctives eSearch'
+ },
+ {
+   'CTname': 'CTM Register (Euromarc)',
+   'OmniaTask': '63155: Correctives IPTOOL'
+ },
+ {
+   'CTname': 'CTM Search Reports Online',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'CTM Watch',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Customer Relationship Management (CRM)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Decision Desktop',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Design Class',
+   'OmniaTask': '65834: Correctives Design Class'
+ },
+ {
+   'CTname': 'Document Access Services',
+   'OmniaTask': '65824: Correctives DAS'
+ },
+ {
+   'CTname': 'Document Management (i.Reach solutions)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Document Management Tools',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Document Management with FileNET P8',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'E-invoicing',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'e-Recruitment / e-Trainees',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'ECAP III',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'eComm',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'ECP5 Capture and Store Historical files',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'EFCA-EIR',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'EFCA-FISHNET',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Electronic Register',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Enforcement Database',
+   'OmniaTask': '65053: Correctives Observatory'
+ },
+ {
+   'CTname': 'ePayment',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'ePlatform',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'eRecruitement SuccesFactors',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Espeak',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'EUAgency Extranet',
+   'OmniaTask': '66552: Correctives EUAgency Extranet'
+ },
+ {
+   'CTname': 'EUIPO Academy Learning Portal',
+   'OmniaTask': '65828: Correctives EUIPO Academy Learning Portal'
+ },
+ {
+   'CTname': 'EUIPO Search Algorithm (OSA)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'EUIPO Similarity Application (OSA2)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Euroclass',
+   'OmniaTask': '65906: Correctives TM Class'
+ },
+ {
+   'CTname': 'Euroclassv2',
+   'OmniaTask': '65906: Correctives TM Class'
+ },
+ {
+   'CTname': 'Eurolocarno',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Eurolocarno WS',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Euromarc++ (EM++)',
+   'OmniaTask': '63155: Correctives IPTOOL'
+ },
+ {
+   'CTname': 'EuroNICE',
+   'OmniaTask': '65830: Correctives EURONICE'
+ },
+ {
+   'CTname': 'Euronice online',
+   'OmniaTask': '65830: Correctives EURONICE'
+ },
+ {
+   'CTname': 'EWS - Admin Tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'EWS - Case Law',
+   'OmniaTask': '67835: Correctives EWS Case Law'
+ },
+ {
+   'CTname': 'EWS - EM Payment Platform',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'EWS - eSearch',
+   'OmniaTask': '65049: Correctives eSearch'
+ },
+ {
+   'CTname': 'EWS - eSearch Mobile',
+   'OmniaTask': '65049: Correctives eSearch'
+ },
+ {
+   'CTname': 'EWS - Harmonized eFiling',
+   'OmniaTask': '66678: Correctives Harmonized eFiling'
+ },
+ {
+   'CTname': 'EWS - Inter partes eFiling',
+   'OmniaTask': '71650: Correctives Inter Partes eFiling'
+ },
+ {
+   'CTname': 'EWS - OPT eFiling',
+   'OmniaTask': '71650: Correctives Inter Partes eFiling'
+ },
+ {
+   'CTname': 'EWS - Portal',
+   'OmniaTask': '65051: Correctives EWS Portal'
+ },
+ {
+   'CTname': 'FileNET Access Component (FNA)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'FindRep Personal Data Management',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'FindRep Search Tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Fisheye/Crucible',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'FSP Back-Office TM and DS',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'FSP e-Filing TM',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'FSP eFiling DS',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'FSP eServices',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'G&S Comparer',
+   'OmniaTask': '65846: Correctives GandS Comparer'
+ },
+ {
+   'CTname': 'Goods & Services Similarity Tool (DIPP)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'HRIS (Allegro HR)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Human Resources Information System SuccessFactors (HRIS SFSF)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Image Processing tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Insite',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Insite (OHIM intranet)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Inter-agency Extranet',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'IP Art Ex parte eFiling',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'IP Enforcement Repository',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'IP Tool',
+   'OmniaTask': '63155: Correctives IPTOOL'
+ },
+ {
+   'CTname': 'IP Toolkit',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'IPTool Helpers',
+   'OmniaTask': '71219: Correctives Helpers'
+ },
+ {
+   'CTname': 'IT IOS request',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'ITD DSS DB request',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'ITD DSS Wiki',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Jira',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Label Management Tool (LMT)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Language checker',
+   'OmniaTask': '68512: Correctives LCT'
+ },
+ {
+   'CTname': 'Microservices',
+   'OmniaTask': '68600: Correctives Microservices'
+ },
+ {
+   'CTname': 'Microsoft Office applications',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Mini Sites Harmonization',
+   'OmniaTask': '68507: Correctives Minisites'
+ },
+ {
+   'CTname': 'Minisites Harmonization',
+   'OmniaTask': '68507: Correctives Minisites'
+ },
+ {
+   'CTname': 'Multi-Platform Communications (MPC)',
+   'OmniaTask': '67838: Correctives MPC'
+ },
+ {
+   'CTname': 'My Page',
+   'OmniaTask': '65051: Correctives EWS Portal'
+ },
+ {
+   'CTname': 'My Service Desk',
+   'OmniaTask': '65055: Correctives MyServiceDesk (Remedy)'
+ },
+ {
+   'CTname': 'Newsletter support (Alicante News)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'OHIM Case-Law (External)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'OHIM E-recruitment',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Online Access to Files (OAF)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Oppo tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Orphan Works',
+   'OmniaTask': '63158: Correctives Orphan Works'
+ },
+ {
+   'CTname': 'Pan-European Seal Talent Bank',
+   'OmniaTask': '70200: Correctives Pan-European Seal Talent Bank'
+ },
+ {
+   'CTname': 'PER - Persons',
+   'OmniaTask': '71901: Correctives PER'
+ },
+ {
+   'CTname': 'President\'s Register',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'QC - Technical Migration',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Quality standards',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'RCD Certificates Online',
+   'OmniaTask': '65049: Correctives eSearch'
+ },
+ {
+   'CTname': 'RCD Database Extraction Engine',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'RCD Download - Web Services',
+   'OmniaTask': '67524: Correctives CTM Download'
+ },
+ {
+   'CTname': 'RCD eFiling',
+   'OmniaTask': '68514: Correctives RCD eFiling'
+ },
+ {
+   'CTname': 'RCD eRenewal',
+   'OmniaTask': '66678: Correctives Harmonized eFiling'
+ },
+ {
+   'CTname': 'RCD Eurolocarno external (classification)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'RCD Eurolocarno internal (classification)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'RCD Examination (EXA)',
+   'OmniaTask': '67523: Correctives RCD EXA'
+ },
+ {
+   'CTname': 'RCD Hague Agreement (HAL)',
+   'OmniaTask': '66096: Correctives HAL'
+ },
+ {
+   'CTname': 'RCD Invalidity',
+   'OmniaTask': '71650: Correctives Inter Partes eFiling'
+ },
+ {
+   'CTname': 'RCD Online',
+   'OmniaTask': '65049: Correctives eSearch'
+ },
+ {
+   'CTname': 'RCD Recordal',
+   'OmniaTask': '66678: Correctives Harmonized eFiling'
+ },
+ {
+   'CTname': 'RCD TRA Monitor',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'RCD Translation',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'RG Support tool',
+   'OmniaTask': '68505: Correctives RG Support Tool'
+ },
+ {
+   'CTname': 'Rollout',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP - General',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP - Inventory (IMSD)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP Basic Component (BC)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP BCM',
+   'OmniaTask': '72438: Correctives SAP BCM CRM'
+ },
+ {
+   'CTname': 'SAP Business Planning & Consolidation (BPC)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP Business Warehouse (BW)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP CRM',
+   'OmniaTask': '72438: Correctives SAP BCM CRM'
+ },
+ {
+   'CTname': 'SAP Finance (FI)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP Human Resources (HR)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP Missions Concur',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SAP Purchasing and Budget (MM/FM)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'ShareDox',
+   'OmniaTask': '63172: Correctives ShareDox'
+ },
+ {
+   'CTname': 'Similarity',
+   'OmniaTask': '69264: Correctives Similarity of GandS'
+ },
+ {
+   'CTname': 'Similarity Feeder',
+   'OmniaTask': '68506: Correctives Similarity Feeder'
+ },
+ {
+   'CTname': 'Similarity of Good and Services',
+   'OmniaTask': '69264: Correctives Similarity of GandS'
+ },
+ {
+   'CTname': 'Software Industrialization',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Sonar',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SP Consolidated Front Office',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SP FO User Area',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'SP Front Office',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'T4T',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Tadcor (Trade Mark and Design Community Online Resources)',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Tagging Tool',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Terminology Maintenance Console (Staging)',
+   'OmniaTask': '63161: Correctives Terminology Maintenance Console (TMC)'
+ },
+ {
+   'CTname': 'Terminology Maintenance Console (TMC)',
+   'OmniaTask': '63161: Correctives Terminology Maintenance Console (TMC)'
+ },
+ {
+   'CTname': 'TM e-filing',
+   'OmniaTask': '65832: Correctives TM eFiling'
+ },
+ {
+   'CTname': 'TM View',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'TMClass',
+   'OmniaTask': '65906: Correctives TM Class'
+ },
+ {
+   'CTname': 'TMDSView',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'TMDSView â€“ Centralized data component',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Trade Marks & Design Repository',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'Translation Gateway (BTJC)',
+   'OmniaTask': '72412: Correctives BTJC'
+ },
+ {
+   'CTname': 'Unified Quality Check Tool Phase 2',
+   'OmniaTask': '65057: Correctives UQCT'
+ },
+ {
+   'CTname': 'Website',
+   'OmniaTask': '65051: Correctives EWS Portal'
+ },
+ {
+   'CTname': 'Windows Migration',
+   'OmniaTask': ''
+ },
+ {
+   'CTname': 'X-marks',
+   'OmniaTask': '65904: Correctives X-Marks'
+ }
+];
