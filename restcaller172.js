11e2a2f7ccfc7e38305629a4a26604f93093039f
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-08 23:58:37 +0100

    e

--- restcaller172.js
+++ restcaller172.js
@@ -209,7 +209,7 @@ function Addduetocard(date,idcard,key,token){
   xhrg.send();
 }
 
-function typeofCT() {
+function typeofCT(arr, cardcreated) {
   var e;
   let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
   width=350,height=400,left=100,top=100`;
