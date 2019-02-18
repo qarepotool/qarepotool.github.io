008a7b17d0dee388d23a5463ad0f1a8bef204bfb
Victor Ruiz <victor.ruiz@ext.euipo.europa.eu>
2019-02-18 16:14:29 +0100

    fix

--- restcaller210.js
+++ restcaller210.js
@@ -27,7 +27,7 @@
     data1.name = document.title;
     var auxtitle=document.title;
     if (document.title.indexOf(auxtitle) !== -1){
-        alert("No se puede crear un card si no se trata de una Control Table")
+        alert("No se puede crear un card si no se trata de una Control Table");
     }
     }
     data1.desc = document.location.href;
